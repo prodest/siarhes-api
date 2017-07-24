6e6dce435305a3b343fb9a81b003b8400f850e1a
Vinicius Avellar <amvinicius@gmail.com>
2017-07-24 10:07:51 -0300

    Correção no docker_build.sh para passar o caminho da instalação oracle

--- .travis/docker_build.sh
+++ .travis/docker_build.sh
@@ -4,4 +4,4 @@ set -ev
 export DOCKER_IMAGE=prodest/api-siarhes:${TRAVIS_COMMIT:0:7}
 
 # Faz o build da imagem Docker.
-docker build -t $DOCKER_IMAGE .
+docker build --build-arg ORACLE_ZIP_DIR=$CACHE_DIR/oracle -t $DOCKER_IMAGE .
