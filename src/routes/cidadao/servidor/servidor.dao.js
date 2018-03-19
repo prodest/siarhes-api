46336a3751ca3952b27a1948bf6a7ece64d8d52e
Marcus Leite <leite.marcus@gmail.com>
2018-03-19 17:19:12 -0300

    Fazendo funcionar pipelined via node.

--- src/routes/cidadao/servidor/servidor.dao.js
+++ src/routes/cidadao/servidor/servidor.dao.js
@@ -34,19 +34,17 @@ module.exports.buscaDadosPessoais = async (conn, cpf) => {
     return rows;
 };
 
-module.exports.buscaDadosTotal = async (conn, cpf) => {
-    const query  = 'BEGIN :ret := U_APISIARHES.PCK_WEB_ACESSO_CID.BUSCA_DADOS_TOTAL; END;';
-    const params = { ret: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT } };
-    
-    let result = await conn.execute(query, params);
+module.exports.buscaDadosTotal = async (conn, cpf) => {    
+    const query  = 'select * from table(u_apisiarhes.pck_web_acesso_cid.busca_dados_total_pipe())';
+    let result = await conn.execute(query, [], { resultSet: true });
     var rows   = new Array();
 
     while (true) {
-        let rowsTmp = await result.outBinds.ret.getRows(10);
+        let rowsTmp = await result.resultSet.getRows(10);
         if (rowsTmp.length == 0) break;
         rows = rows.concat(rowsTmp);
     }
 
-    await result.outBinds.ret.close();
+    await result.resultSet.close();
     return rows;
 };
